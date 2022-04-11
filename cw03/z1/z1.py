422781ac1a9246ce66a69e4314917bfeb540b9db
mateu <s19282@pjwskt.edu.pl>
2022-04-11 13:38:42 +0200

    progress

---
+++ cw03/z1/z1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+file = pd.read_csv('../PAD_03_PD.csv')
+file['Country'].agg
+print(file['Country'])
--- cw04/z4/z4.py
+++ cw04/z4/z4.py
@@ -4,4 +4,9 @@ import pandas as pd
 df = pd.read_csv('Zadanie_4.csv', delimiter=";")
 df = df.iloc[:, :-4]
 df['DateTime'] = pd.to_datetime(df['DateTime'])
-print(df.groupby([df['DateTime'].dt.day, 'DoctorID', 'Type', 'City']).head())
+df['Date'] = pd.to_datetime(df['DateTime'].dt.date)
+print(df.groupby(['Date', 'DoctorID', 'Type', 'City']).first())
+print(df.groupby(['Date', 'DoctorID', 'Type', 'City']).count())
+# print(df.groupby(['Date', 'DoctorID', 'Type', 'City']).count().first())
+
+# print(df.groupby([pd.Grouper(key='DateTime', freq='2D'), 'DoctorID', 'Type', 'City']).head())
