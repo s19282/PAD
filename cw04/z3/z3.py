fcfdd6ee341ea2033bcdae13ed9462342a6ced38
mateu <s19282@pjwskt.edu.pl>
2022-04-11 14:06:04 +0200

    updated z3

--- cw04/z3/z3.py
+++ cw04/z3/z3.py
@@ -3,6 +3,6 @@ import numpy as np
 a = np.genfromtxt('Zadanie_3_macierz_A.csv', delimiter=',', skip_header=False)
 b = np.genfromtxt('Zadanie_3_macierz_B.csv', delimiter=',', skip_header=False)
 
-c = np.matrix.sum(a * b)/np.sqrt(np.matrix.sum(a**2)*np.sqrt(np.matrix.sum(b**2)))
+c = np.dot(a, b.T)/(np.linalg.norm(a)*np.linalg.norm(b))
 print(c)
 
