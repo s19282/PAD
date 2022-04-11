b638efa36876fc604c10cce28f559728a699592d
mateusz <s19282@pjwstk.edu.pl>
2022-04-11 10:11:59 +0200

    z4 not ready v2

--- cw04/z4/z4.py
+++ cw04/z4/z4.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
 
-m = np.genfromtxt('Zadanie_4.csv', delimiter=';', skip_header=False)
 df = pd.read_csv('Zadanie_4.csv', delimiter=";")
-
-
-df2 = df.groupby(['DateTime', 'DoctorID'])
-print(df)
+df = df.iloc[:, :-4]
+df['DateTime'] = pd.to_datetime(df['DateTime'])
+print(df.groupby([df['DateTime'].dt.day, 'DoctorID', 'Type', 'City']).head())
