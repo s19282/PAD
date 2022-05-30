55302d4e84b226a9649407f5da9c05bf5c9ceed9
mateu <s19282@pjwskt.edu.pl>
2022-05-30 21:37:34 +0200

    v1

--- cw08/z1_z2.py
+++ cw08/z1_z2.py
@@ -1,9 +1,12 @@
 import pandas as pd
-import scipy as sp
-import plotly.express as px
-import statsmodels as sm
+import statsmodels.formula.api as smf
 
 df = pd.read_csv('PAD_09_PD.csv', delimiter=";")
 income_df = df.iloc[:, [0, 2]]
 
-model = sm.ols
+model = smf.ols(formula='Annual_Income ~ Gender', data=income_df).fit()
+print(model.summary())
+# pvalues = model.pvalues
+# max_p = pvalues.idxmax()
+# alpha = 0.05
+# while pvalues
