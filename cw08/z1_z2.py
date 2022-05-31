8e8c707eae6235ab2804f2fa1a690a6e189bfb2d
mateu <s19282@pjwskt.edu.pl>
2022-05-31 21:26:59 +0200

    z1

--- cw08/z1_z2.py
+++ cw08/z1_z2.py
@@ -1,12 +1,18 @@
 import pandas as pd
-import statsmodels.formula.api as smf
+import statsmodels.api as smf
+from statsmodels.stats.weightstats import ttest_ind
+# z1
 
 df = pd.read_csv('PAD_09_PD.csv', delimiter=";")
 income_df = df.iloc[:, [0, 2]]
+male = income_df.query('Gender == "Male"')['Annual_Income']
+female = income_df.query('Gender == "Female"')['Annual_Income']
+res = ttest_ind(male, female)
+# p value > 0,05 nie możemy odrzucić hipotezy zerowej
+print(res)
 
-model = smf.ols(formula='Annual_Income ~ Gender', data=income_df).fit()
-print(model.summary())
-# pvalues = model.pvalues
-# max_p = pvalues.idxmax()
-# alpha = 0.05
-# while pvalues
+# 2
+
+
+# model = smf.ols(formula='Annual_Income ~ Gender', data=income_df).fit()
+# print(model.summary())
