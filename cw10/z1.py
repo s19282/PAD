7a601af9f213044a830987d9193deba5f6fa8f34
mateusz <s19282@pjwstk.edu.pl>
2022-06-18 14:48:38 +0200

    z1_v1.1

--- cw10/z1.py
+++ cw10/z1.py
@@ -13,24 +13,23 @@ selected = option_menu(
     default_index=0,
     orientation='horizontal'
 )
-
-if selected == 'Stats':
-    data = sl.file_uploader("Load csv", type=['csv'])
-    if data is not None:
-        with sl.spinner("Loading..."):
-            df = pd.read_csv(data)
-            sl.dataframe(df)
-        selected_visualization = sl.selectbox("Select visualisation", ("Type of clothes", "Gender of client"))
-        container = sl.container()
-        if selected_visualization == "Type of clothes":
-            fig = px.histogram(df['tshirt_type'], x='tshirt_type', title="Clothes type")
-            container.plotly_chart(fig, use_container_width=True)
-        elif selected_visualization == "Gender of client":
-            fig = px.histogram(df['gender'], x='gender', title="Client's gender")
-            container.plotly_chart(fig, use_container_width=True)
 if selected == 'Questionnaire':
     firstname = sl.text_input("First Name")
     lastname = sl.text_input("Last Name")
     if sl.button('Send'):
         result = 'Questionnaire saved for: {} {}'.format(firstname.title(), lastname.title())
         sl.success(result)
+if selected == 'Stats':
+    data = sl.file_uploader("Load csv", type=['csv'])
+    if data is not None:
+        with sl.spinner("Loading..."):
+            df = pd.read_csv(data)
+            sl.dataframe(df)
+        selected_visualization = sl.selectbox("Select visualisation", ("Type of clothes", "Tshirt quantity"))
+        container = sl.container()
+        if selected_visualization == "Type of clothes":
+            fig = px.histogram(df['tshirt_type'], x='tshirt_type', title="Clothes type")
+            container.plotly_chart(fig, use_container_width=True)
+        elif selected_visualization == "Tshirt quantity":
+            fig = px.histogram(df['tshirt_quantity'], x='tshirt_quantity', title="Tshirt quantity")
+            container.plotly_chart(fig, use_container_width=True)
