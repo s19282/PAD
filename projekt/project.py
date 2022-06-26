89448275533a779b14f2cb7e23c785e3e7216c8e
mateusz <s19282@pjwstk.edu.pl>
2022-06-26 22:06:29 +0200

    v2 beautifulsoup read first page

--- projekt/project.py
+++ projekt/project.py
@@ -1,60 +1,76 @@
-import time
-
 import pandas as pd
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
+import re
 
 # z2
 baseurl = "https://www.otomoto.pl/osobowe"
-xpaths = {
-    'offers': "//article[@class='ooa-rld5ij e1b25f6f18']",
-    'cookies': "//button[@id='onetrust-accept-btn-handler']",
-    'lastPage': "//a[@href='/osobowe']",
-    'offer': "//li[@class='offer-params__item']",
-}
-vehicles = pd.DataFrame(
-    columns=['seller type', 'make', 'model', 'year', 'mileage', 'fuel', 'horse power', 'transmission', 'drive train',
-             'body type', 'seats', 'doors', 'color', 'country', 'accident free', 'seller name', 'location'])
-myDriver = webdriver.Chrome()
-myDriver.maximize_window()
-myDriver.get(baseurl)
-myDriver.find_element(by=By.XPATH, value=xpaths['cookies']).click()
-# lastPageNumber = myDriver\
-#     .find_element(by=By.XPATH, value=xpaths['lastPage'])\
-#     .find_element(by=By.CSS_SELECTOR, value='span').get_property("value")
-offers = myDriver.find_elements(by=By.CSS_SELECTOR, value="article")
-delay = 3
-print("1")
-offers[1].click()
-print("1.1")
-myDriver.back()
-print("2")
+
+# vehicles = pd.DataFrame(
+#     columns=['seller type', 'make', 'model', 'version', 'year', 'mileage', 'capacity', 'fuel', 'horse power',
+#              'transmission', 'drive train', 'damaged', 'body type', 'seats', 'doors', 'color', 'first owner',
+#              'serviced at aso', 'state', 'country', 'accident free', 'seller name', 'location'])
+
+vehicles = pd.DataFrame()
+
+page = requests.get(baseurl)
+soup = BeautifulSoup(page.text, 'html.parser')
+offers = soup.select('article div h2 a[href]')
+
 for offer in offers:
-    tmp = offer
-    try:
-        print("1")
-        tmp.click()
-        print("2")
-        myElem = WebDriverWait(myDriver, delay).until(EC.presence_of_element_located(tmp))
-        print("3")
-    except TimeoutException:
-        print("Loading took too much time!")
+    offerLink = offer['href']
+    offerHTML = requests.get(offerLink).text
+    vehicle = BeautifulSoup(offerHTML, 'html.parser')
+    data = vehicle.select('ul li.offer-params__item')
 
-    print("2")
-    break
-    # try:
-    #     WebDriverWait(myDriver, delay).until(EC.presence_of_element_located(offer.click()))
-    # except TimeoutException:
-    #     print
-    #     "Loading took too much time!"
-    # print("1")
-    # # offer.click()
-    # print("2")
+    newVehicle = {}
 
-    # # print(offer.find_element(by=By.XPATH, value=xpaths['offer']).find_element(by=By.CSS_SELECTOR, value="div a")
-    # #        .get_attribute("value"))
-    # myDriver.back()
+    for param in data:
+        value = ''
+        try:
+            value = param.select('div a')[0].text.strip()
+        except IndexError:
+            value = param.select('div')[0].text.strip()
+
+        match param.select('span')[0].text:
+            case 'Oferta od':
+                newVehicle['seller type'] = value
+            case 'Marka pojazdu':
+                newVehicle['make'] = value
+            case 'Model pojazdu':
+                newVehicle['model'] = value
+            case 'Wersja':
+                newVehicle['version'] = value
+            case 'Rok produkcji':
+                newVehicle['year'] = value
+            case 'Pojemność skokowa':
+                newVehicle['capacity'] = value
+            case 'Rodzaj paliwa':
+                newVehicle['fuel'] = value
+            case 'Moc':
+                newVehicle['horse power'] = value
+            case 'Skrzynia biegów':
+                newVehicle['transmission'] = value
+            case 'Napęd':
+                newVehicle['drive train'] = value
+            case 'Uszkodzony':
+                newVehicle['damaged'] = value
+            case 'Typ nadwozia':
+                newVehicle['body type'] = value
+            case 'Liczba drzwi':
+                newVehicle['doors'] = value
+            case 'Liczba miejsc':
+                newVehicle['seats'] = value
+            case 'Kolor':
+                newVehicle['color'] = value
+            case 'Pierwszy właściciel':
+                newVehicle['first owner'] = value
+            case 'Serwisowany w ASO':
+                newVehicle['serviced at aso'] = value
+            case 'Stan':
+                newVehicle['state'] = value
+
+    vehicles = vehicles.append(newVehicle, ignore_index=True)
+print(vehicles)
+vehicles.to_csv('offers.csv', index=False, encoding='utf-8')
 
