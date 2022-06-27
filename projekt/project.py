4878adf277d81e47700aa6f09affbfc04404c542
mateusz <s19282@pjwstk.edu.pl>
2022-06-27 20:05:41 +0200

    scrap page by page

--- projekt/project.py
+++ projekt/project.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import re
+from random import randint
+from time import sleep
+import os
 
 # z2
-baseurl = "https://www.otomoto.pl/osobowe"
-
+if os.path.exists('offers.csv'):
+    os.remove('offers.csv')
+baseurl = "https://www.otomoto.pl/osobowe?page="
+pageNumber = 0
 # vehicles = pd.DataFrame(
 #     columns=['seller type', 'make', 'model', 'version', 'year', 'mileage', 'capacity', 'fuel', 'horse power',
 #              'transmission', 'drive train', 'damaged', 'body type', 'seats', 'doors', 'color', 'first owner',
@@ -13,64 +17,78 @@ baseurl = "https://www.otomoto.pl/osobowe"
 
 vehicles = pd.DataFrame()
 
-page = requests.get(baseurl)
-soup = BeautifulSoup(page.text, 'html.parser')
-offers = soup.select('article div h2 a[href]')
+while True:
+    pageNumber = pageNumber + 1
+    print("Page: ", pageNumber)
+    page = requests.get(f"{baseurl}{pageNumber}")
+    # sleep(randint(1, 10))
+    soup = BeautifulSoup(page.text, 'html.parser')
+    offers = soup.select('article div h2 a[href]')
+    if pageNumber % 50 == 0:
+        vehicles.to_csv('offers.csv', encoding='utf-8')
 
-for offer in offers:
-    offerLink = offer['href']
-    offerHTML = requests.get(offerLink).text
-    vehicle = BeautifulSoup(offerHTML, 'html.parser')
-    data = vehicle.select('ul li.offer-params__item')
+    try:
+        for offer in offers:
+            offerLink = offer['href']
+            offerHTML = requests.get(offerLink).text
+            vehicle = BeautifulSoup(offerHTML, 'html.parser')
+            data = vehicle.select('ul li.offer-params__item')
 
-    newVehicle = {}
+            newVehicle = {}
 
-    for param in data:
-        value = ''
-        try:
-            value = param.select('div a')[0].text.strip()
-        except IndexError:
-            value = param.select('div')[0].text.strip()
+            for param in data:
+                value = ''
+                try:
+                    value = param.select('div a')[0].text.strip()
+                except IndexError:
+                    value = param.select('div')[0].text.strip()
 
-        match param.select('span')[0].text:
-            case 'Oferta od':
-                newVehicle['seller type'] = value
-            case 'Marka pojazdu':
-                newVehicle['make'] = value
-            case 'Model pojazdu':
-                newVehicle['model'] = value
-            case 'Wersja':
-                newVehicle['version'] = value
-            case 'Rok produkcji':
-                newVehicle['year'] = value
-            case 'Pojemność skokowa':
-                newVehicle['capacity'] = value
-            case 'Rodzaj paliwa':
-                newVehicle['fuel'] = value
-            case 'Moc':
-                newVehicle['horse power'] = value
-            case 'Skrzynia biegów':
-                newVehicle['transmission'] = value
-            case 'Napęd':
-                newVehicle['drive train'] = value
-            case 'Uszkodzony':
-                newVehicle['damaged'] = value
-            case 'Typ nadwozia':
-                newVehicle['body type'] = value
-            case 'Liczba drzwi':
-                newVehicle['doors'] = value
-            case 'Liczba miejsc':
-                newVehicle['seats'] = value
-            case 'Kolor':
-                newVehicle['color'] = value
-            case 'Pierwszy właściciel':
-                newVehicle['first owner'] = value
-            case 'Serwisowany w ASO':
-                newVehicle['serviced at aso'] = value
-            case 'Stan':
-                newVehicle['state'] = value
+                match param.select('span')[0].text:
+                    case 'Oferta od':
+                        newVehicle['seller type'] = value
+                    case 'Marka pojazdu':
+                        newVehicle['make'] = value
+                    case 'Model pojazdu':
+                        newVehicle['model'] = value
+                    case 'Wersja':
+                        newVehicle['version'] = value
+                    case 'Rok produkcji':
+                        newVehicle['year'] = value
+                    case 'Pojemność skokowa':
+                        newVehicle['capacity'] = value
+                    case 'Rodzaj paliwa':
+                        newVehicle['fuel'] = value
+                    case 'Moc':
+                        newVehicle['horse power'] = value
+                    case 'Skrzynia biegów':
+                        newVehicle['transmission'] = value
+                    case 'Napęd':
+                        newVehicle['drive train'] = value
+                    case 'Uszkodzony':
+                        newVehicle['damaged'] = value
+                    case 'Typ nadwozia':
+                        newVehicle['body type'] = value
+                    case 'Liczba drzwi':
+                        newVehicle['doors'] = value
+                    case 'Liczba miejsc':
+                        newVehicle['seats'] = value
+                    case 'Kolor':
+                        newVehicle['color'] = value
+                    case 'Pierwszy właściciel':
+                        newVehicle['first owner'] = value
+                    case 'Serwisowany w ASO':
+                        newVehicle['serviced at aso'] = value
+                    case 'Stan':
+                        newVehicle['state'] = value
 
-    vehicles = vehicles.append(newVehicle, ignore_index=True)
-print(vehicles)
-vehicles.to_csv('offers.csv', index=False, encoding='utf-8')
+            newVehicle['price'] = vehicle.select('span.offer-price__number')[0].text.strip()
+            newVehicle['currency'] = vehicle.select('span.offer-price__number span.offer-price__currency')[0].text.strip()
+            newVehicle['priceDetails'] = vehicle.select('span.offer-price__details')[0].text.strip()
+            newVehicle['location'] = vehicle.select('a.seller-card__links__link__cta')[0].text.strip()
+            newVehicle['description'] = vehicle.select('div.offer-description__description')[0].text.strip()
 
+            vehicles = vehicles.append(newVehicle, ignore_index=True)
+
+    except Exception:
+        vehicles.to_csv('offers.csv', encoding='utf-8')
+        break
