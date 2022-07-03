185003ed60e4d0efdc2e3442a915d95bba4df6bd
mateusz <s19282@pjwstk.edu.pl>
2022-07-03 22:57:39 +0200

    analyse year - mileage

--- projekt/analyseData.py
+++ projekt/analyseData.py
@@ -1,6 +1,9 @@
+from statistics import mode
+from matplotlib.pyplot import title
 import pandas as pd
 import statsmodels.formula.api as smf
 import plotly.express as px
+import plotly.graph_objects as go
 
 df = pd.read_csv('carsPrepared.csv')
 
@@ -17,20 +20,23 @@ yearPrice['year'] = df.year
 yearPrice['price'] = df.price
 
 yearAVGPrice = yearPrice.groupby(['year']).mean().reset_index()
-fig = px.scatter(yearAVGPrice, "year", "price", title="Average price of vehicle grouped by year")
+fig = go.Figure()
+fig.add_trace(
+        go.Scatter(x=yearAVGPrice["year"], y=yearAVGPrice["price"], mode="markers"))
+fig.update_layout(title="Average price of vehicle grouped by year", xaxis_title="Year")
 fig.show()
 
-fig = px.scatter(df, "year", "price", title="Vehicle price depending on the year of production")
+yearMileage = pd.DataFrame()
+yearMileage['year'] = df.year
+yearMileage['mileage'] = df.mileage
+
+yearAVGMileage = yearMileage.groupby(['year']).mean().reset_index()
+fig = go.Figure()
+fig.add_trace(
+        go.Scatter(x=yearAVGMileage["year"], y=yearAVGMileage["mileage"], mode="markers"))
+fig.update_layout(title="Average mileage of vehicle grouped by year", xaxis_title="Year")
 fig.show()
 
-yearPrice = pd.DataFrame()
-yearPrice['year'] = df.year
-yearPrice['mileage'] = df.mileage
-
-mileageAVGPrice = yearPrice.groupby(['year']).mean().reset_index()
-fig = px.scatter(mileageAVGPrice, "year", "mileage", title="Average mileage of vehicle grouped by year")
-fig.show()
-
-fig = px.scatter(df.iloc[:, [14, 6]].groupby(['price']).mean().reset_index(), "price", "horsePower",
-                 title="Average horse power of vehicle grouped by price")
-fig.show()
+# fig = px.scatter(df.iloc[:, [14, 6]].groupby(['price']).mean().reset_index(), "price", "horsePower",
+#                  title="Average horse power of vehicle grouped by price")
+# fig.show()
